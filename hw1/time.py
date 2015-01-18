f822c5e6d8d0221c015409ed3839b8f492bc6977
Chuphay <chuphay@gmail.com>
2015-01-17 17:19:57 -0800

    being stupid

--- hw1/time.py
+++ hw1/time.py
@@ -1,8 +1,10 @@
-import os
+import os, sys
 
 os.system('(time ./bilateralfilter < images/cat.pgm) 2> time.txt')
 text = open('time.txt').read().split()
 data = {text[0][4:]:text[0][:4],text[1][4:]:text[1][:4],text[2][7:]:text[2][2:7]}
 for i in data:
-    print i, data[i]
+    print( i, data[i])
+
+print(sys.argv[1])
 
