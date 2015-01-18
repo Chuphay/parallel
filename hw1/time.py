389384cdd685c68918477270f06e553319867b4d
chuphay <chuphay@gmail.com>
2015-01-17 18:34:59 -0800

    finished the speed test on trig

--- hw1/time.py
+++ hw1/time.py
@@ -5,10 +5,18 @@ data = {'elapsed':[], 'user':[], 'system':[]}
 for i in range(10):
     os.system('(time ./bilateralfilter < images/cat.pgm) 2> time.txt')
     text = open('time.txt').read().split()
-    
+
+    """
+    #for the 336 machines:
     data[text[0][4:]].append(text[0][:4])
     data[text[1][4:]].append(text[1][:4])
     data[text[2][7:]].append(text[2][3:7])
+    """
+    #for the trig machines:
+    data['elapsed'].append(text[1])
+    data['user'].append(text[3])
+    data['system'].append(text[5])
+    #print text
 
 
 try:
