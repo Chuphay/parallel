51b5dd5e035d41b7a318ac42c8bed485ad9fc287
chuphay <chuphay@gmail.com>
2015-01-17 17:45:16 -0800

    got the speed test working

--- hw1/time.py
+++ hw1/time.py
@@ -1,10 +1,27 @@
 import os, sys
 
-os.system('(time ./bilateralfilter < images/cat.pgm) 2> time.txt')
-text = open('time.txt').read().split()
-data = {text[0][4:]:text[0][:4],text[1][4:]:text[1][:4],text[2][7:]:text[2][2:7]}
-for i in data:
-    print( i, data[i])
+data = {'elapsed':[], 'user':[], 'system':[]}
 
-print(sys.argv[1])
+for i in range(10):
+    os.system('(time ./bilateralfilter < images/cat.pgm) 2> time.txt')
+    text = open('time.txt').read().split()
+
+    data[text[0][4:]].append(text[0][:4])
+    data[text[1][4:]].append(text[1][:4])
+    data[text[2][7:]].append(text[2][3:7])
+
+
+try:
+    text = open(sys.argv[1],'w')
+    for i in data:
+        text.write(i)
+        text.write('\n')
+        for j in data[i]:
+            text.write(j)
+            text.write(', ')
+        text.write('\n')
+    text.close()
+except IndexError:
+    print 'supply a name for a file to save the data to.'
+    pass
 
