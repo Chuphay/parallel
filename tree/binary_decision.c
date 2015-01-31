4ee5e4203c3c363dc123cd4874f2ea94a1690993
Chuphay <chuphay@gmail.com>
2015-01-30 21:22:05 -0800

    coming along

--- tree/binary_decision.c
+++ tree/binary_decision.c
@@ -26,11 +26,10 @@ double entropy(double q){
     printf("entropy of %f?\n",q);
     exit(1);
   }
-
   if((q == 0) || (q == 1))
     return 0;
   else 
-    return -(q * log(q) + (1-q)*log(1-q));
+    return -(q * log(q)/log(2) + (1-q)*log(1-q)/log(2));
 }
 
 double get_prob(int *target, int length){
@@ -42,6 +41,36 @@ double get_prob(int *target, int length){
   return out/length;
 }
 
+double get_prob_a(int *input, int *target, int length){
+
+  int i;
+  int pos_pos = 0;
+  int pos_neg = 0;
+  int neg_pos = 0;
+  int neg_neg = 0;
+  for(i = 0; i<length; i++){
+    if((input[i] == 1) && (target[i] == 1))
+      pos_pos++;
+    if((input[i] == 1) && (target[i] == 0))
+      pos_neg++;
+    if((input[i] == 0) && (target[i] == 1))
+      neg_pos++;
+    if((input[i] == 0) && (target[i] == 0))
+      neg_neg++;
+  }
+  printf("pos_pos: %d pos_neg: %d \n", pos_pos, pos_neg);
+  printf("neg_pos: %d neg_neg: %d\n", neg_pos, neg_neg);
+
+
+  //apparently the most important equation is this:
+
+  // \sum_{k=1}^2 \frac {p_k+n_k} {p+n} entropy(\frac {p_k} {p_k+n_k}).
+
+  return 0.0;
+
+
+
+}
 
 
 
@@ -67,6 +96,18 @@ int main(){
 
 
     printf("prob %f\n", get_prob(&data[3][4], 4));
+
+    get_prob_a(data[0],data[3],8);
+    get_prob_a(data[1],data[3],8);
+    get_prob_a(data[2], data[3],8);
+
+
+  printf("entropy of 0.5 %f\n", entropy(0.5));
+
+  //apparently the most important equation is this:
+
+  // \sum_{k=1}^2 \frac {p_k+n_k} {p+n} entropy(\frac {p_k} {p_k+n_k}).
+
 
   return 0;
 }
